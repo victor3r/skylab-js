5906530c1751b4d595127c84c73f4dc809cf1503
victor <victorabelo1697@gmail.com>
2020-05-19 15:31:53 -0300

    Iniciando aplicação

--- todo/todos.js
+++ todo/todos.js
@@ -1,1 +1,9 @@
-alert('Funcionou');
+const listElement = document.querySelector('#app ul');
+const inputElement = document.querySelector('#app input');
+const buttonElement = document.querySelector('#app button');
+
+const todos = [
+  'Fazer café',
+  'Estudar Javascript',
+  'Acessar comunidade da Rocketseat'
+];
